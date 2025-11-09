923babc8065049f35bfbfb059306069ebfcfcf0e
Cyd Stumpel <info@cydstumpel.nl>
2025-11-09 12:53:05 +0100

    done ✨

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Workshop Wey Wey Web
+This is the boilerplate for the workshop I created for [Wey Wey Web](https://weyweyweb.com/)!
+
 
 ## Running this project
 This project is built in CSS, JS and HTML, no bundlers involved. You can either open the index.html in your browser or use a tool like [Live Server](https://open-vsx.org/extension/ritwickdey/LiveServer)
 
+## Generating pages
+Updating all the speaker pages or speaker overview data by hand is a lot of work, that's why I created a generate function that generates the index.html with speaker overview and the speaker-detail pages automatically from the people-data.js in the generate-html folder. 
+
+The pages are created by the generate-pages.js script inside the generate-html folder which replaces some strings with the data from people-data.js in the  detail-template.html and index-template.html. Feel free to add your own people!
+
+When you're done editing the template html files run `npm run generate-pages`!
--- generate-html/generate-pages.js
+++ generate-html/generate-pages.js
@@ -3,11 +3,48 @@ import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { mkdirSync, rmSync } from 'fs'
 import { people } from './people-data.js'
-import { createList } from './components/people-list.js'
-
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
+
+
+const createList = () => {
+  let listItems = createPeople();
+
+  return `
+  <ul class="people-list">
+  ${listItems}
+  </ul>
+`;
+}
+
+const createPeople = () => {
+  let peopleItems = '';
+
+  people.forEach(person => {
+    let personSlug = person.id
+    const randomRotation = Math.random() * 10 - 5;
+    peopleItems += `
+    <li class="person-item" style="--random-rotation: ${randomRotation}deg;">
+      <div class="person-item__image" style="--vt: ${personSlug};">
+        <img src="${person.media}" alt="${person.name}" loading="eager" class="person-image" width="1600" height="2400">
+      </div>
+      <a href="./people/${personSlug}">
+        <div class="person-info" style="--vt: ${personSlug}-info;">
+          <h3 class="person-name large-body">
+          ${person.name}
+          </h3>
+          <p class="person-title default-body">${person.title}</p>
+        </div>
+      </a>
+    </li>
+    `;
+  });
+
+  return peopleItems;
+}
+
+
 
 // Function to generate speaker pages
 function generatePersonPages() {
